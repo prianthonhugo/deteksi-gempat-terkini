03cec5147303cfc487578d2ff93651e259eae359
prianthonhugo <prianthonsubardiodanhugo@gmail.com>
2024-09-21 12:39:10 +0700

    final version

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ Aplikasi deteksi gempa terkini
 Modularisasi dengan function
 Modularisasi dengan package
 """
+# from gempaterkini import GempaTerkini
 import gempaterkini
-from gempaterkini import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
-    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {gempaterkini.description}')
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    gempa_di_indonesia = gempaterkini.GempaTerkini('https://bmkg.go.id/')
+    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {gempa_di_indonesia.description}')
+    gempa_di_indonesia.tampilkan_keterangan()
+    gempa_di_indonesia.run()
