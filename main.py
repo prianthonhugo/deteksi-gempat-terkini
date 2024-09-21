a34eb0a797dde784f94254de34b6d36daa10ceb8
prianthonhugo <prianthonsubardiodanhugo@gmail.com>
2024-09-21 12:12:36 +0700

    finish real data from bmkg.go.id

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ Aplikasi deteksi gempa terkini
 Modularisasi dengan function
 Modularisasi dengan package
 """
+import gempaterkini
 from gempaterkini import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
-    print('Aplikasi utama')
+    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {gempaterkini.description}')
     result = ekstraksi_data()
     tampilkan_data(result)
